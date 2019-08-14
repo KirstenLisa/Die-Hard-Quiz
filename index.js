9af50e55fe7cae08714b3f9c1ed4cc7c2c087975
Kirsten Kustusch <kirsten.kustusch@gmail.com>
2019-08-14 16:18:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+function startQuiz() {
+    /* starts quiz when start-button clicked*/
+  }
+  
+  function questionPage() {
+    /* generates div with questions and answer options
+    user has to choose one option
+    then click submit-button */
+  }
+  
+  function checkAnswer() {
+    /* takes user input and compares it with correctAnswer
+    if user input correct => corrAnswer()
+    if user input wrong => wrongAnswer() */
+  }
+  
+  function corrAnswer() {
+    /* generates div for corrAnswer
+    updates score */
+  }
+  
+  function wrongAnswer() {
+    /* generates div for wrongAnswer */
+  }
+  
+  function nextQuestion() {
+    /* generates div for next question 
+    updates questionNumber */
+  }
+  
+  function finalScore() {
+    /* generates div for end result
+    has restart-button */
+  }
+  
+  function restartquiz() {
+    /* reloads quiz again */
+  }
+  
+  $(function quiz() {
+    /* contains all functions */
+  })
+  
+  
