af40f0c9d5951562c2c0e9068a9db278e2e9dc60
Kirsten Kustusch <kirsten.kustusch@gmail.com>
2019-08-25 23:14:52 +0300

    question and header changes

--- store.js
+++ store.js
@@ -1,44 +1,43 @@
 const STORE = [ 
-    {
-      question: 'What is John McClane\'s catchphrase?', answers: ['Ding-dong-doo!', 'Abracadabra!', 'Yippee-kai-yay!', 'Olly-olly-olly!'], correctAnswer: 'Yippee-kai-yay!', pic: 'https://storiesthatreallymattered.files.wordpress.com/2009/01/diehard982734.jpg', alt: 'John McClane'
-      },
-    
-    {
-      question: 'The first Die Hard movie takes place on which day?', answers: ['Christmas Eve', 'Halloween', 'May Day Bank Holiday', 'Good Friday'], correctAnswer: 'Christmas Eve', pic: 'https://media.socastsrm.com/wordpress/wp-content/blogs.dir/671/files/2018/07/die-hard-christmas.jpg', alt: 'Santa Claus'
-    },
-  
-    {
-      question: 'Who was considered for the role of John McClane and declined it?', answers: ['Charly Sheen', 'Bruce Lee', 'Vin Diesel', 'Frank Sinatra'], correctAnswer: 'Frank Sinatra', pic: 'https://www.billboard.com/files/styles/article_main_image/public/media/frank-sinatra-1963-billboard-650.jpg', alt: 'Frank Sinatra'
-    },
-  
-    {
-      question: 'Which language is villain Hans Gruber supposed to speak?', answers: ['Arabic', 'Russian', 'German', 'Swedish'], correctAnswer: 'German', pic: 'https://i.pinimg.com/originals/87/fa/83/87fa830f9b762c31b08b7791ae06914e.jpg', alt: 'Hans Gruber'
-    },
-  
-    {
-      question: 'What is the name of John McClane\'s wife?', answers: ['Holly', 'Polly', 'Annie', 'Lauren'], correctAnswer: 'Holly', pic: 'https://static3.srcdn.com/wordpress/wp-content/uploads/2018/12/Die-Hard-2-John-and-Holly-McClane.jpg', alt: 'Holly and John McClane'
-    },
-  
-    {
-      question: 'Where takes the second part of Die Hard place?', answers: ['Streets of LA', 'Workplace of John McClane\'s wife', 'Airport', 'Burger King'], correctAnswer: 'Airport', pic: 'https://images.tbs.com/tbs/w_1024/https%3A%2F%2Fi.cdn.tbs.com%2Fassets%2Fimages%2F2017%2F08%2FDieHard2-1600x900.jpg', alt: 'McClane and snow' 
+  {
+    question: 'What is John McClane\'s catchphrase?', answers: ['Ding-dong-doo!', 'Abracadabra!', 'Yippee-kai-yay!', 'Olly-olly-olly!'], correctAnswer: 'Yippee-kai-yay!', pic: 'https://storiesthatreallymattered.files.wordpress.com/2009/01/diehard982734.jpg', alt: 'John McClane'
     },
   
   {
-    question: 'What was Samuel L. Jackson\'s Die Hard character called?', answers: ['Apollo', 'Zeus', 'Mars', 'Poseidon'], correctAnswer: 'Zeus', pic: 'http://www.cineimage.ch/db_data/movies/diehard3/scen/tumblr_mbjlzdk9qR1qbewsxo1_1280.jpg', alt: 'Zeus and McClane'
+    question: 'The first Die Hard movie takes place on which day?', answers: ['Christmas Eve', 'Halloween', 'May Day Bank Holiday', 'Good Friday'], correctAnswer: 'Christmas Eve', pic: 'https://media.socastsrm.com/wordpress/wp-content/blogs.dir/671/files/2018/07/die-hard-christmas.jpg', alt: 'Santa Claus'
   },
-  
+
   {
-    question: 'What game did the villain want police to play in Die Hard With A Vengeance?', answers: ['Simon Says', 'Twister', 'Trivial Pursuit', 'Sudoko'], correctAnswer: 'Simon Says', pic: 'https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:49684?quality=0.8&format=jpg', alt: 'Zeus and McClane on bridge'
+    question: 'Who was considered for the role of John McClane and declined it?', answers: ['Charly Sheen', 'Bruce Lee', 'Vin Diesel', 'Frank Sinatra'], correctAnswer: 'Frank Sinatra', pic: 'https://www.billboard.com/files/styles/article_main_image/public/media/frank-sinatra-1963-billboard-650.jpg', alt: 'Frank Sinatra'
   },
-  
+
   {
-    question: 'Who plays hacker Matt Farrell in Die Hard 4.0?', answers: ['Colin Farell', 'Robert Pattinson', 'Seth Rogen','Justin Long'], correctAnswer: 'Justin Long', pic: 'https://movieevangelist.files.wordpress.com/2013/02/die-hard-4.jpg', alt:'McClane and Farrell'
-  
+    question: 'Which language is villain Hans Gruber supposed to speak?', answers: ['Arabic', 'Russian', 'German', 'Swedish'], correctAnswer: 'German', pic: 'https://i.pinimg.com/originals/87/fa/83/87fa830f9b762c31b08b7791ae06914e.jpg', alt: 'Hans Gruber'
   },
-  
+
   {
-    question: 'Where does Die Hard 5 take place?', answers: ['Moscow', 'New York', 'Cairo', 'Berlin'], correctAnswer: 'Moscow', pic: 'https://static01.nyt.com/images/2013/02/14/arts/good-span/good-span-jumbo.jpg', alt: 'Die Hard 5'
-  }
-  ];
-  
-  
+    question: 'What is the name of John McClane\'s wife?', answers: ['Holly', 'Polly', 'Annie', 'Lauren'], correctAnswer: 'Holly', pic: 'https://static3.srcdn.com/wordpress/wp-content/uploads/2018/12/Die-Hard-2-John-and-Holly-McClane.jpg', alt: 'Holly and John McClane'
+  },
+
+  {
+    question: 'Where takes the second part of Die Hard place?', answers: ['Streets of LA', 'NAkatomi Plaza', 'Airport', 'Burger King'], correctAnswer: 'Airport', pic: 'https://images.tbs.com/tbs/w_1024/https%3A%2F%2Fi.cdn.tbs.com%2Fassets%2Fimages%2F2017%2F08%2FDieHard2-1600x900.jpg', alt: 'McClane and snow' 
+  },
+
+{
+  question: 'What is the name of Samuel L. Jackson\'s Die Hard character?', answers: ['Apollo', 'Zeus', 'Mars', 'Poseidon'], correctAnswer: 'Zeus', pic: 'http://www.cineimage.ch/db_data/movies/diehard3/scen/tumblr_mbjlzdk9qR1qbewsxo1_1280.jpg', alt: 'Zeus and McClane'
+},
+
+{
+  question: 'What game did the villain want police to play in Die Hard With A Vengeance?', answers: ['Simon Says', 'Twister', 'Trivial Pursuit', 'Sudoko'], correctAnswer: 'Simon Says', pic: 'https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:49684?quality=0.8&format=jpg', alt: 'Zeus and McClane on bridge'
+},
+
+{
+  question: 'Who plays hacker Matt Farrell in Die Hard 4.0?', answers: ['Colin Farell', 'Robert Pattinson', 'Seth Rogen','Justin Long'], correctAnswer: 'Justin Long', pic: 'https://movieevangelist.files.wordpress.com/2013/02/die-hard-4.jpg', alt:'McClane and Farrell'
+
+},
+
+{
+  question: 'Where does Die Hard 5 take place?', answers: ['Moscow', 'New York', 'Cairo', 'Berlin'], correctAnswer: 'Moscow', pic: 'https://static01.nyt.com/images/2013/02/14/arts/good-span/good-span-jumbo.jpg', alt: 'Die Hard 5'
+}
+];
+
